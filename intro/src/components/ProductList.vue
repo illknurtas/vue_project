fb5b6114089d305def913865290428c179418882
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-03-18 18:43:26 +0300

    update product listing

--- intro/src/components/ProductList.vue
+++ intro/src/components/ProductList.vue
@@ -1,7 +1,30 @@
 <template>
     <div>
-        <h3>Product List</h3>
-        {{ products.length }}
+        <p v-if="products.length == 0">
+            Product list is empty!
+        </p>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Product Name</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                    <th>Unit Price</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products" :key="product.id">
+                    <td>{{ product.id }}</td>
+                    <td>{{ product.productName }}</td>
+                    <td>{{ product.categoryId }}</td>
+                    <td>{{ product.quantityPerUnit }}</td>
+                    <td>{{ product.unitPrice }}</td>
+                    <td>{{ product.unitsInStock }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 <script>
